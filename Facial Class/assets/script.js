5c43121426340f8be331e3209b3ab3c5df750e7b
Ruan Richard <155588491+luruanx@users.noreply.github.com>
2025-05-21 21:54:34 -0300

    created facial class

---
+++ Facial Class/assets/script.js
@@ -0,0 +1,28 @@
+// Initialize Swiper
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 18,
+    },
+  },
+});
